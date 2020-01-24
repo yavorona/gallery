22686a50000d640b7c9b5317e62b22703467f7ba
Kevin Gutierrez <kevin.w.gutierrez@gmail.com>
2020-01-24 11:08:25 -0800

    initialized seed script

---
+++ client/src/components/App.jsx
@@ -0,0 +1,15 @@
+// main app file
+
+import React from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div style={divStyle}> App</div>;
+  }
+}
+
+export default App;
---
+++ client/src/index.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReacDOM from "react-dom";
+
+ReactDOM.render(<App />, document.getElementById("app"));
