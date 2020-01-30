6dced6267e2b2fa41391a22bda059407321027db
Kevin Gutierrez <kevin.w.gutierrez@gmail.com>
2020-01-29 17:07:33 -0800

    edit listObjects functionality

--- server/helpers.js
+++ server/helpers.js
@@ -1,24 +1,24 @@
-const config = require("./config.js");
+const credentials = require("../aws/credentials.js");
 const AWS = require("aws-sdk");
-const s3 = new AWS.S3({
-  accessKeyId: config.accessKey,
-  secretAccessKey: config.secretKey,
-  region: "us-west-1"
-});
-
-module.exports.s3 = s3;
+const S3 = new AWS.S3();
 
 // function to get pictures from whichever location is inputted
+
 let fetchPhotos = (location, cb) => {
-  // parameters that specify the S3 bucket and folder (location) to grab photos of
-  var params = {
-    Bucket: "fecgallery", // bucket name,
-    // EncodingType: "url",
-    MaxKeys: 20,
-    Key: `${location}`
-  };
+  AWS.config.update({
+    accessKeyId: credentials.accessKey,
+    secretAccessKey: credentials.secretKey
+  });
+  AWS.config.region = "us-west-1";
+  var s3Bucket = new AWS.S3({
+    params: {
+      Bucket: "fecgallery",
+      MaxKeys: 10,
+      Key: `${location}`
+    }
+  });
   var photoArray = [];
-  s3.listObjectsV2(params, (err, data) => {
+  s3Bucket.listObjectsV2((err, data) => {
     if (err) {
       return err;
     } else {
@@ -26,14 +26,14 @@ let fetchPhotos = (location, cb) => {
       contents.forEach(content => {
         photoArray.push(content.Key);
       });
-      if (data.IsTruncated) {
-        params.ContinuationToken = data.NextContinuationToken;
-        console.log("getting rest of keys");
-        fetch();
-      }
+      // if (data.IsTruncated) {
+      //   params.ContinuationToken = data.NextContinuationToken;
+      //   console.log("getting rest of keys");
+      //   fetch();
+      // }
     }
     cb(photoArray);
   });
 };
 
-module.exports = fetchPhotos;
+module.exports.fetch = fetchPhotos;
