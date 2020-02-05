80297650a254b0e89f6571c8c243500692dd2410
Kevin Gutierrez <kevin.w.gutierrez@gmail.com>
2020-02-05 11:59:04 -0800

    code clean up

--- server/index.js
+++ server/index.js
@@ -6,25 +6,18 @@ const parser = require("body-parser");
 const app = express();
 const port = 3000;
 const db = require("./db.js");
-const { fetch } = require("./helpers");
-// const controller = require("./controller.js");
 
 app.use("/:id/", express.static(DIST_DIR));
 app.use(parser.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.get("/hotels/:id/", (req, res) => {
-  console.log(req.params);
+app.get("/gallery/:id/", (req, res) => {
   let galleryId = req.params.id;
   return db.findOne({ id: galleryId }).then(photos => {
     res.send(photos);
   });
 });
-// fetch("cambodia", data => {
-//   console.log("server recieved: ", data);
-//   res.send(data);
-// });
 
 app.listen(port, () => {
   console.log(`Express listening on ${port}`);
--- server/seed.js
+++ server/seed.js
@@ -1,93 +1,61 @@
-// file for seed script for photo gallery
-
 const Gallery = require("./db.js");
-
-// hardcoded url seed
 const imageUrls = require("./urls.js");
 
-// generate random number for which hotel to display
 const randomNum = () => {
   return Math.floor(Math.random() * 5) + 1;
 };
 
-// grab hotel based on random number (1-5)
-let getRandomHotel = (num, cb) => {
-  // if (num === 1) {
-  //   fetch("null", data => {
-  //     console.log("seed recieved: ", data);
-  //     cb(data);
-  //   });
-  // }
-  // if (num === 2) {
-  //   fetch("costarica", data => {
-  //     console.log("seed recieved: ", data);
-  //     cb(data);
-  //   });
-  // }
-  // if (num === 3) {
-  //   fetch("italy", data => {
-  //     console.log("seed recieved: ", data);
-  //     cb(data);
-  //   });
-  // }
-  // if (num === 4) {
-  //   fetch("singapore", data => {
-  //     console.log("seed recieved: ", data);
-  //     cb(data);
-  //   });
-  // }
-  // if (num === 5) {
-  //   fetch("tokyo", data => {
-  //     console.log("seed recieved: ", data);
-  //     cb(data);
-  //   });
-  // }
-};
-
-// establish 100 photo galleries with corresponding arrays based on random number
-let seed1 = () => {
-  for (var i = 0; i < 100; i++) {
-    // let random = randomNum();
-    // let diningArray = [];
-    // let roomsArray = [];
-    // let travelerArray = [];
-    // let mainArray = [];
-    // getRandomHotel(random, data => {
-    //   console.log(random);
-    //   console.log(data);
-    //   data.forEach(img => {
-    //     if (img.includes("main")) {
-    //       mainArray.push(img);
-    //     }
-    //     if (img.includes("roomsuite")) {
-    //       roomsArray.push(img);
-    //     }
-    //     if (img.includes("traveler")) {
-    //       travelerArray.push(img);
-    //     }
-    //     if (img.includes("dining")) {
-    //       diningArray.push(img);
-    //     }
-    //   });
-    // });
-    // console.log(
-    //   "dining:",
-    //   diningArray,
-    //   "main: ",
-    //   mainArray,
-    //   "rooms: ",
-    //   roomsArray,
-    //   "traveler: ",
-    //   travelerArray
-    // );
-    // db.Gallery.create({
-    //   id: i,
-    //   main: mainArray,
-    //   dining: diningArray,
-    //   rooms: roomsArray,
-    //   traveler: travelerArray
-    // });
+// assigning hardcoded s3 urls for each hotel
+for (var i = 0; i < 100; i++) {
+  let randomInt = randomNum();
+  if (randomInt === 1) {
+    let gallery = new Gallery({
+      id: i,
+      main: imageUrls.cambodia.main,
+      dining: imageUrls.cambodia.dining,
+      roomsuite: imageUrls.cambodia.roomSuite,
+      traveler: imageUrls.cambodia.traveler
+    });
+    gallery.save();
   }
-};
-
-module.exports.seed1 = seed1;
+  if (randomInt === 2) {
+    let gallery = new Gallery({
+      id: i,
+      main: imageUrls.costarica.main,
+      dining: imageUrls.costarica.dining,
+      roomsuite: imageUrls.costarica.roomSuite,
+      traveler: imageUrls.costarica.traveler
+    });
+    gallery.save();
+  }
+  if (randomInt === 3) {
+    let gallery = new Gallery({
+      id: i,
+      main: imageUrls.italy.main,
+      dining: imageUrls.italy.dining,
+      roomsuite: imageUrls.italy.roomSuite,
+      traveler: imageUrls.italy.traveler
+    });
+    gallery.save();
+  }
+  if (randomInt === 4) {
+    let gallery = new Gallery({
+      id: i,
+      main: imageUrls.singapore.main,
+      dining: imageUrls.singapore.dining,
+      roomsuite: imageUrls.singapore.roomSuite,
+      traveler: imageUrls.singapore.traveler
+    });
+    gallery.save();
+  }
+  if (randomInt === 5) {
+    let gallery = new Gallery({
+      id: i,
+      main: imageUrls.tokyo.main,
+      dining: imageUrls.tokyo.dining,
+      roomsuite: imageUrls.tokyo.roomSuite,
+      traveler: imageUrls.tokyo.traveler
+    });
+    gallery.save();
+  }
+}
